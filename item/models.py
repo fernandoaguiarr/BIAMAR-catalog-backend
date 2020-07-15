7dac319de367949f2ff0d39960b38b136f89c8a0
FusedBaggins <faguiar408@gmail.com>
2020-07-15 15:45:45 -0300

    change model structure from mongoDB to postegreSQL

--- item/models.py
+++ item/models.py
@@ -1,101 +1,186 @@
-from djongo import models
+from django.db import models
+from django.contrib.postgres.fields import ArrayField, JSONField
 
 
-# ABSTRACT MODELS
-# ITEM ABSTRACT MODELS
-
-class Generic(models.Model):
-    id = models.IntegerField()
-    name = models.CharField(max_length=255, verbose_name="Período")
+class Size(models.Model):
+    description = models.CharField(
+        max_length=4,
+        null=False,
+        verbose_name="Tamanho"
+    )
 
     class Meta:
-        abstract = True
+        verbose_name = "Grade"
+        verbose_name_plural = "Grades"
 
-    def __str__(self):
-        return str(self.id)
+    def __str__(self): return self.description
+
+
+class Color(models.Model):
+    readonly_fields = ('id',)
+
+    id = models.CharField(
+        primary_key=True,
+        auto_created=False,
+        max_length=32,
+        null=False,
+        verbose_name="Código"
+    )
+
+    name = models.CharField(
+        max_length=32,
+        null=False,
+        verbose_name="Nome"
+    )
+
+    class Meta:
+        verbose_name = "Cor"
+        verbose_name_plural = "Cores"
+
+    def __str__(self): return self.name
+
+
+class Type(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        auto_created=False,
+        null=False,
+
+        verbose_name="Código"
+    )
+
+    name = models.CharField(
+        max_length=64,
+        null=False,
+        verbose_name="Nome"
+    )
+
+    class Meta:
+        verbose_name = "Tipo de Produto"
+        verbose_name_plural = "Tipos de Produtos"
+
+    def __str__(self): return self.name
+
+
+class Season(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        auto_created=False,
+        null=False,
+
+        verbose_name="Código"
+    )
+
+    name = models.CharField(
+        max_length=64,
+        null=False,
+        verbose_name="Nome"
+    )
+
+    class Meta:
+        verbose_name = "Coleção"
+        verbose_name_plural = "Coleções"
+
+    def __str__(self): return self.name
+
+
+class Brand(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        auto_created=False,
+        null=False,
+
+        verbose_name="Código"
+    )
+
+    name = models.CharField(
+        max_length=64,
+        null=False,
+        verbose_name="Nome"
+    )
+
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
+
+    def __str__(self): return self.name
+
+
+class Item(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=16,
+        null=False
+    )
+
+    group = models.CharField(
+        max_length=16,
+        null=False
+    )
+
+    genre = models.CharField(
+        max_length=16,
+        null=True,
+    )
+    price = models.CharField(max_length=8, null=True)
+    type = models.ForeignKey(Type, related_name="item_type", on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, related_name="brand_type", on_delete=models.CASCADE)
+    season = models.ForeignKey(Season, related_name="season_type", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Itens"
+
+    def __str__(self): return self.id
 
 
 class Sku(models.Model):
-    class Color(models.Model):
-        id = models.CharField(max_length=64)
-        name = models.CharField(max_length=64, verbose_name="nome")
+    id = models.CharField(
+        primary_key=True,
+        max_length=8,
+    )
 
-        class Meta:
-            abstract = True
+    ean = models.CharField(
+        max_length=24,
+        null=True,
+    )
 
-        def __str__(self):
-            return str(self.id)
+    weight = models.CharField(
+        max_length=6,
+        null=False
+    )
 
-    class AdditionalField(models.Model):
-        id = models.IntegerField()
-        title = models.CharField(max_length=64, verbose_name="nome")
-        description = models.CharField(max_length=64, verbose_name="descrição")
-
-        class Meta:
-            abstract = True
-            verbose_name_plural = "Campos Adicionais"
-
-        def __str__(self):
-            return str(self.id)
-
-    id = models.CharField(max_length=255)
-    sku = models.IntegerField()
-    price = models.CharField(max_length=8, verbose_name="preço")
-    weight = models.CharField(max_length=8, verbose_name="Peso")
-    size = models.CharField(max_length=64, verbose_name="numeração")
-
-    color = models.EmbeddedField(model_container=Color, verbose_name="Cor")
-    additional_field = models.ArrayField(model_container=AdditionalField, verbose_name="campo adicional")
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    id_item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
     class Meta:
-        abstract = True
+        verbose_name = "SKU"
+        verbose_name_plural = "SKUs"
 
-    def __str__(self):
-        return str(self.id)
+    def __str__(self): return self.id
 
 
-class Specs(models.Model):
-    id = models.IntegerField()
-    id_description = models.IntegerField(verbose_name="id descrição")
-    name = models.CharField(max_length=64, verbose_name="nome")
-    description = models.CharField(max_length=64, verbose_name="descrição")
+class TypePhoto(models.Model):
+    name = models.CharField(max_length=32, null=False)
 
     class Meta:
-        abstract = True
+        verbose_name = "Tipo de Foto"
+        verbose_name_plural = "Tipos de Fotos"
 
-    def __str__(self):
-        return str(self.id)
-
-
-# NON ABSTRACT MODELS
-class Item(models.Model):
-    _id = models.ObjectIdField()  # This is used to avoid calling makemigrations/migrate every changes
-    id = models.CharField(max_length=64, verbose_name="Referência")
-    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Preço")
-
-    brand = models.EmbeddedField(model_container=Generic, verbose_name="Marca")
-    collection = models.EmbeddedField(model_container=Generic, verbose_name="Coleção")
-    type = models.EmbeddedField(model_container=Generic, verbose_name="Tipo")
-    genre = models.EmbeddedField(model_container=Generic, verbose_name="Genero")
-
-    sku = models.ArrayField(model_container=Sku)
-    specs = models.ArrayField(model_container=Specs, verbose_name="Classificação")
-
-    objects = models.DjongoManager()
-
-    class Meta:
-        verbose_name_plural = "Itens"
-
-    def __str__(self):
-        return self.id
+    def __str__(self): return self.name
 
 
 class Photo(models.Model):
-    _id = models.ObjectIdField()  # This is used to avoid calling makemigrations/migrate every changes
-    id = models.CharField(max_length=32)
-    photos = models.JSONField(null=True, blank=True)
+    type = models.ForeignKey(TypePhoto, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    path = models.CharField(max_length=56)
 
-    objects = models.DjongoManager()
+    preview = models.BooleanField()
 
-    def __str__(self):
-        return self.id
+    class Meta:
+        verbose_name = "Foto"
+        verbose_name_plural = "Fotos"
+
+    def __str__(self): return str(self.id) + " - " + str(self.color)
