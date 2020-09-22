5e801c64d6460b7ceb3f860ae41eeefff20d6e2b
FusedBaggins <faguiar408@gmail.com>
2020-09-22 11:12:18 -0300

    feat: add support for upload PDF files

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from core.models import VirtualAgeToken
+from core.models import VirtualAgeToken, Pdf
 
 
 @admin.register(VirtualAgeToken)
 class VirtualAgeTokenAdmin(admin.ModelAdmin):
     fields = ('code', 'date')
+
+
+@admin.register(Pdf)
+class PdfAdmin(admin.ModelAdmin):
+    readonly_fields = ['url_tag']
+    fieldsets = [("PDF", {'fields': ['name']}), ("Link", {'fields': ['file']})]
--- core/models.py
+++ core/models.py
@@ -1,9 +1,14 @@
+import os
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 
 # Create your models here.
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
+from django.utils.safestring import mark_safe
 from rest_framework.authtoken.models import Token
 
 
@@ -32,3 +37,32 @@ class VirtualAgeToken(models.Model):
         db_table = "core_token_virtual_age"
         verbose_name = "Token Virtual Age"
         verbose_name_plural = "Tokens Virtual Age"
+
+
+def upload(instance, filename):
+    return "pdf/{0}.{1}".format('_'.join(instance.name.split(' ')), filename.split('.')[-1])
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, **kwargs):
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
+
+
+class Pdf(models.Model):
+    def url_tag(self):
+        return mark_safe(
+            '<span>{}{}<span/>'.format('https://biamar.com.br/', self.file.name)) if self.file else ""
+
+    name = models.CharField(max_length=256, null=False, blank=False)
+    file = models.FileField(upload_to=upload, storage=OverwriteStorage)
+    url_tag.short_description = 'Url'
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        app_label = "core"
+        verbose_name = "PDF"
+        verbose_name_plural = "PDFs"
