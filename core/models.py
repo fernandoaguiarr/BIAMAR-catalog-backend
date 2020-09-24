798d76c44554d10f530bc55a85ee0d4c63da67e8
FusedBaggins <faguiar408@gmail.com>
2020-09-24 09:52:15 -0300

    feat: add field in PDF model

--- core/models.py
+++ core/models.py
@@ -40,7 +40,7 @@ class VirtualAgeToken(models.Model):
 
 
 def upload(instance, filename):
-    return "pdf/{0}.{1}".format('_'.join(instance.name.split(' ')), filename.split('.')[-1])
+    return "pdf/{0}_{1}.{2}".format('_'.join(instance.name.split(' ')), instance.year, filename.split('.')[-1])
 
 
 class OverwriteStorage(FileSystemStorage):
@@ -53,11 +53,12 @@ class OverwriteStorage(FileSystemStorage):
 class Pdf(models.Model):
     def url_tag(self):
         return mark_safe(
-            '<span>{}{}<span/>'.format('https://biamar.com.br/', self.file.name)) if self.file else ""
+            '<span>{}{}<span/>'.format('https://biamar.com.br/', self.file.name)) if self.file else "Unavailable :("
 
-    name = models.CharField(max_length=256, null=False, blank=False)
+    name = models.CharField(max_length=256, null=False, blank=False, unique=False)
+    year = models.IntegerField(null=False, blank=False, unique=False)
     file = models.FileField(upload_to=upload, storage=OverwriteStorage)
-    url_tag.short_description = 'Url'
+    url_tag.short_description = 'path'
 
     def __str__(self):
         return self.name
@@ -66,3 +67,12 @@ class Pdf(models.Model):
         app_label = "core"
         verbose_name = "PDF"
         verbose_name_plural = "PDFs"
+
+        ordering = ['-year']
+
+
+@receiver(post_delete, sender=Pdf)
+def delete_old_photo(sender, instance, **kwargs):
+    if os.path.isfile(instance.file.path):
+        os.remove(instance.file.path)
+    return False
