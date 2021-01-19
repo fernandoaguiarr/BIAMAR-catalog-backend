e8f0a37af2f8c154c29577979afaf54b6a664248
FusedBaggins <faguiar408@gmail.com>
2021-01-19 15:16:01 -0300

    refactor(utils): change PDF Admin app (manager -> utils)

--- utils/admin.py
+++ utils/admin.py
@@ -1,1 +1,12 @@
+from django.contrib import admin
 
+from utils.models import Pdf
+
+
+@admin.register(Pdf)
+class PdfAdmin(admin.ModelAdmin):
+    readonly_fields = ['url_tag']
+    fieldsets = [("PDF", {'fields': ['name', 'year', 'file']}), ("Link", {'fields': ['url_tag']})]
+
+    list_display = ['year', 'name']
+    search_fields = ['name', 'year']
