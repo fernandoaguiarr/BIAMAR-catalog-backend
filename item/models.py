e6c55085bba7f43d23abba1280ed7ee908a5d2de
FusedBaggins <faguiar408@gmail.com>
2021-01-19 14:46:45 -0300

    fix: remove unused signals imports

--- item/models.py
+++ item/models.py
@@ -4,8 +4,6 @@ from uuid import uuid4
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.conf import settings
-from django.db.models.signals import post_save, pre_save, post_delete
-from django.dispatch import receiver
 from django.utils.safestring import mark_safe
 
 
