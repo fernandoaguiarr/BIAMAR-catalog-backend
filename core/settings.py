256c389552ce5552f6b92f53bda5f3a71606430c
fusedbaggins <faguiar408@gmail.com>
2021-08-30 16:09:24 -0300

    feat: initial commit for utils app

--- core/settings.py
+++ core/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'item.apps.ItemConfig',
+    'utils.apps.UtilsConfig',
 ]
 
 MIDDLEWARE = [
