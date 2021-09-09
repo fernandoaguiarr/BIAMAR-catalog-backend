2eafa0bcc5bf8ce6bfd800ae0dbb7ea3f8cf7928
fusedbaggins <faguiar408@gmail.com>
2021-09-09 15:29:04 -0300

    feat: register custom MailNotification admin page

--- utils/admin.py
+++ utils/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from utils.models import MailNotification
+
 
 # Register your models here.
+# admin.site.register(MailNotification)
+
+
+@admin.register(MailNotification)
+class MailNotificationAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Query code', {'fields': ('code',)}),
+        ("Notification's info", {'fields': ('name', 'description')}),
+        ('Send to', {'fields': ('users',)})
+    )
+
+    filter_horizontal = ('users',)
+    readonly_fields = ('code',)
+    search_fields = ('code', 'name')
