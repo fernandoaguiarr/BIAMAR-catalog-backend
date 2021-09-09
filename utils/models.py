ab4d148ddfb25798a82e0ee6144b55bb2d208f0d
fusedbaggins <faguiar408@gmail.com>
2021-09-09 15:28:36 -0300

    feat: add MailNotification model

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,15 @@
+import uuid
+
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+class MailNotification(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    code = models.UUIDField(default=uuid.uuid4())
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    users = models.ManyToManyField(User)
