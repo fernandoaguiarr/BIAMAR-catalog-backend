7a7919b3904452f7285d0c0fdbb3d9e0342c41b2
fusedbaggins <faguiar408@gmail.com>
2021-09-10 08:37:48 -0300

    feat: add command to send emails

---
+++ utils/management/commands/sendmail.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.mail import send_mail
+from django.core.management import BaseCommand
+
+from utils.models import MailNotification
+
+
+class Command(BaseCommand):
+    def __init__(self):
+        super().__init__()
+        self._email_params = ['subject', 'message', 'html_message', 'recipient_list', 'from_email']
+
+    def add_arguments(self, parser):
+        parser.add_argument('notification_code', type=str)
+        parser.add_argument('subject', type=str)
+        parser.add_argument('message', type=str)
+        parser.add_argument('html_message', type=str)
+
+    def handle(self, *args, **options):
+        try:
+            notification = MailNotification.objects.get(code=options['notification_code'])
+            params = dict(zip(self._email_params, list(options.values())[8:]))
+            params['recipient_list'] = notification.users
+            params['from_email'] = settings.DEFAULT_FROM_EMAIL
+
+            send_mail(**params)
+
+        except ObjectDoesNotExist:
+            pass
