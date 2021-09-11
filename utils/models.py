4c9207c34bdc8500354d3e75806d545050070ed1
fusedbaggins <faguiar408@gmail.com>
2021-09-11 09:14:38 -0300

    fix: remove call signature uuid4

--- utils/models.py
+++ utils/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class MailNotification(models.Model):
     id = models.BigAutoField(primary_key=True)
-    code = models.UUIDField(default=uuid.uuid4())
+    code = models.UUIDField(default=uuid.uuid4)
     name = models.CharField(max_length=32)
     description = models.TextField()
     users = ArrayField(
