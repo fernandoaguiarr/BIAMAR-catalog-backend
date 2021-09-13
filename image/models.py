678599466b31932dead400a7bb848cad7a7447e4
fusedbaggins <faguiar408@gmail.com>
2021-09-13 07:34:22 -0300

    feat: add export_to field on photo model

--- image/models.py
+++ image/models.py
@@ -1,9 +1,22 @@
 import uuid
 
+from django import forms
+from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 # Create your models here.
 from item.models import Group, Color
+
+
+class ExportForChoiches(models.TextChoices):
+    VTEX = '1', _('VTEX')
+    PRESTASHOP = '2', _('PrestaShop')
+    CATALOGO = '3', _('Catálogo')
+
+
+def get_export_for_default():
+    return [ExportForChoiches.PRESTASHOP, ExportForChoiches.CATALOGO]
 
 
 class Category(models.Model):
@@ -18,3 +31,8 @@ class Photo(models.Model):
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     file = models.ImageField(width_field=1332, height_field=1767, max_length=128)
+    export_to = ArrayField(
+        models.CharField(blank=True, null=True, max_length=32),
+        choices=ExportForChoiches.choices,
+        default=get_export_for_default
+    )
