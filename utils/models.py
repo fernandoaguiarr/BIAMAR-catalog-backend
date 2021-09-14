b7cc995cba616c1ef7a9b26440d96bd2d01c4bca
fusedbaggins <faguiar408@gmail.com>
2021-09-14 08:58:09 -0300

    feat: set ExportFor model

--- utils/models.py
+++ utils/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import UniqueConstraint
 from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
@@ -19,3 +20,17 @@ class MailNotification(models.Model):
         constraints = [
             UniqueConstraint(fields=['code'], name='unique_notification_code')
         ]
+
+
+class ExportForTypes(models.IntegerChoices):
+    IMAGE = (1, _('Imagens'))
+    SKU = (2, _('Sku'))
+
+
+class ExportFor(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=32)
+    category = models.IntegerField(choices=ExportForTypes.choices)
+
+    def __str__(self):
+        return self.name
