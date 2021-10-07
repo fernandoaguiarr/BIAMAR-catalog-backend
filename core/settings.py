ea0ff1349c3825d589acfe3e4013b05a9f6cf697
fusedbaggins <faguiar408@gmail.com>
2021-10-07 07:55:44 -0300

    feat: install cors headers module

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'item.apps.ItemConfig',
     'utils.apps.UtilsConfig',
     'image.apps.ImageConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
