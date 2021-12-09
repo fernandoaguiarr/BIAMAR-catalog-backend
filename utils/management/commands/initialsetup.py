bba3bc1c7f76d438502dc2db4b8d0cb5c79aec9b
fusedbaggins <faguiar408@gmail.com>
2021-12-09 15:53:21 -0300

    refactor(utils): change permissions insertions method

--- utils/management/commands/initialsetup.py
+++ utils/management/commands/initialsetup.py
@@ -1,33 +1,68 @@
 from decouple import config
 from django.contrib.auth.models import Permission, User
 from django.contrib.contenttypes.models import ContentType
-from django.core.management import BaseCommand, call_command
+from django.core.management import BaseCommand
 
 from image.models import Category
 from utils.models import ExportFor
+
+
+def generate_permissions_values(model_name, action_name, extra):
+    return {
+        'codename': f'{action_name}_{model_name}{"_" if extra else ""}{"_".join(extra)}',
+        'name': f'Can {action_name} {model_name}{" " if extra else ""} {" ".join(extra)}'
+    }
 
 
 class Command(BaseCommand):
 
     @staticmethod
     def create_permissions():
-        Permission.objects.get_or_create(
-            codename='can_view_sku_availability',
-            name='Can view sku availability',
-            content_type=ContentType.objects.get(app_label='item', model='sku')
-        )
+        objects = [
+            {
+                **generate_permissions_values('sku', 'get', ['api']),
+                **{'content_type': ContentType.objects.get(app_label='item', model='sku')}
+            },
+            {
+                **generate_permissions_values('sku', 'get', ['api', 'availability', 'field']),
+                **{'content_type': ContentType.objects.get(app_label='item', model='sku')}
+            },
+            {
+                **generate_permissions_values('item', 'get', ['api']),
+                **{'content_type': ContentType.objects.get(app_label='item', model='item')}
+            },
+            {
+                **generate_permissions_values('item', 'get', ['api', 'price', 'field']),
+                **{'content_type': ContentType.objects.get(app_label='item', model='item')}
+            },
+            {
+                **generate_permissions_values('group', 'get', ['api']),
+                **{'content_type': ContentType.objects.get(app_label='item', model='group')}
+            },
+            {
+                **generate_permissions_values('category', 'get', ['api']),
+                **{'content_type': ContentType.objects.get(app_label='item', model='category')}
+            },
+            {
+                **generate_permissions_values('brand', 'get', ['api']),
+                **{'content_type': ContentType.objects.get(app_label='item', model='brand')}
+            },
+            {
+                **generate_permissions_values('season', 'get', ['api']),
+                **{'content_type': ContentType.objects.get(app_label='item', model='season')}
+            },
+            {
+                **generate_permissions_values('photo', 'get', ['api']),
+                **{'content_type': ContentType.objects.get(app_label='image', model='photo')}
+            },
+            {
+                **generate_permissions_values('photo', 'get', ['api', 'categories']),
+                **{'content_type': ContentType.objects.get(app_label='image', model='photo')}
+            },
+        ]
 
-        Permission.objects.get_or_create(
-            codename='can_view_item_price',
-            name='Can view item price',
-            content_type=ContentType.objects.get(app_label='item', model='item')
-        )
-
-        Permission.objects.get_or_create(
-            codename='can_view_all_photos',
-            name='Can view all photos',
-            content_type=ContentType.objects.get(app_label='image', model='photo')
-        )
+        for obj in objects:
+            Permission.objects.get_or_create(**obj)
 
     @staticmethod
     def create_crontab_jobs():
