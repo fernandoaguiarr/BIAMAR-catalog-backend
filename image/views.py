67b7bbbb988ea40e55ea8388d1536c990e9dbb10
fusedbaggins <faguiar408@gmail.com>
2021-12-10 07:43:26 -0300

    feat: add permissions to viewsets

--- image/views.py
+++ image/views.py
@@ -2,14 +2,15 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
 from item.models import Group, Color
 from image.models import Photo, Category
 from utils.interfaces import CustomViewSet
 from image.serializers import PhotoSerializer
+from image.permissions import DefaultViewSetPermission
 
 
-# Create your views here.
 class PhotoViewSet(viewsets.ViewSet, CustomViewSet):
 
     def __init__(self, **kwargs):
@@ -18,11 +19,14 @@ class PhotoViewSet(viewsets.ViewSet, CustomViewSet):
             'color': {'klass': Color, 'query_key': 'ERP_id'},
             'category': {'klass': Category, 'query_key': 'id'},
         }
-        super().__init__(filters=filters, **kwargs)
+        CustomViewSet.__init__(self, filters=filters)
 
     @staticmethod
     def get_queryset():
         return Photo.objects.all()
+
+    def get_permissions(self):
+        return [IsAuthenticated(), DefaultViewSetPermission()]
 
     def list(self, request, *args, **kwargs):
         query_params = request.query_params.copy()
@@ -30,6 +34,8 @@ class PhotoViewSet(viewsets.ViewSet, CustomViewSet):
 
         if query_params:
             queryset = queryset.filter(**self.get_filter_object(query_params))
+            if not request.user.has_perm('image.get_photo_api_categories'):
+                queryset = queryset.exclude(export_to__isnull=True)
 
         serializer = PhotoSerializer(queryset, many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
